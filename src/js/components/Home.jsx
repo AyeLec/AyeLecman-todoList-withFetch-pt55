1b5576ee4bd0d3ed3b21517670f37f68381fcae2
Aye Lecman <ayelenlecman@gmail.com>
2025-07-29 01:18:20 +0000

    agregado de actualización con la api

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback  } from "react";
 import TasksList from "./TasksList";
 
 const USER = "aye_lecman";
@@ -7,7 +7,7 @@ const Home = () => {
 	const [tasks, setTasks] = useState([]);
 	const [error, setError] = useState("");
 
-	useEffect(() => {
+	const loadTasks = useCallback(() => {
 		fetch(`https://playground.4geeks.com/todo/users/${USER}`)
 			.then(res => {
 				if (!res.ok) {
@@ -27,8 +27,12 @@ const Home = () => {
 					setTasks([]);
 				}
 			})
-			.catch(err => setError("Error al verificar o crear usuario"));
-	}, []);
+			.catch(() => setError("Error al verificar o crear usuario"));
+	}, []); //
+
+	useEffect(() => {
+		loadTasks(); 
+	}, [loadTasks]);
 
 	const addTask = (newTaskText) => {
 		const text = newTaskText.trim();
@@ -41,20 +45,20 @@ const Home = () => {
 		};
 
 		fetch(`https://playground.4geeks.com/todo/todos/${USER}`, requestOptions)
-			.then(res => res.json())
-			.then(newTask => {
-				setTasks(prev => [...prev, newTask]);
+			.then(res => {
+				if (!res.ok) throw new Error();      
+				loadTasks();                          
 			})
-			.catch(err => setError("Error al agregar tarea"));
+			.catch(() => setError("Error al agregar tarea")); 
 	};
 
 	const handleDelete = (taskId) => {
 		fetch(`https://playground.4geeks.com/todo/todos/${taskId}`, { method: "DELETE" })
 			.then(res => {
-				if (!res.ok) throw new Error("Error al borrar");
-				setTasks(prev => prev.filter(t => t.id !== taskId));
+				if (!res.ok) throw new Error();      
+				loadTasks();                          
 			})
-			.catch(err => setError("Error al borrar tarea"));
+			.catch(() => setError("Error al borrar tarea")); 
 	};
 
 	const deleteAllTasks = () => {
@@ -72,14 +76,12 @@ const Home = () => {
 			.then(responses => {
 				const allOk = responses.every(res => res.ok);
 				if (allOk) {
-					setTasks([]); 
+					loadTasks(); 
 				} else {
 					setError("Algunas tareas no se pudieron borrar");
 				}
 			})
-			.catch(err => {
-				setError("Error al borrar tareas");
-			});
+			.catch(() => setError("Error al borrar tareas")); 
 	};
 
 	return (
